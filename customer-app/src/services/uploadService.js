173d108969022f0201787ae0f09c08d386c22da1
Aravinth <aravinthmgt63@gmail.com>
2026-08-14 20:25:15 +0530

    phone num credientials fix

--- customer-app/src/services/uploadService.js
+++ customer-app/src/services/uploadService.js
@@ -1,36 +1,3 @@
-// import { supabase } from "./supabase/supabaseClient";
-
-// export async function createUpload(uploadData) {
-
-//   // Generate upload number
-//   const { data: numberData, error: numberError } =
-//     await supabase.rpc("generate_upload_number");
-
-//   if (numberError) {
-//     throw numberError;
-//   }
-
-//   const finalData = {
-//     ...uploadData,
-//     upload_number: numberData,
-//   };
-
-
-//   // Insert
-//   const { data, error } = await supabase
-//     .from("uploads")
-//     .insert([finalData])
-    
-
-//   if (error) {
-//     throw error;
-//   }
-
-
-//   return data;
-// }
-
-
 import { supabase } from "./supabase/supabaseClient";
 
 export async function createUpload(uploadData) {
